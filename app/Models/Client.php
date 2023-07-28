fe26422131aafc99232c5bbbef81567c6b3b6e7e
Alberto <betoangel100@gmail.com>
2023-07-27 19:40:11 -0600

    adding the fields with massive fill information to our Model Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+    //aqui se suele definir los campos que suelen rellenarse de forma masiva
+    protected $fillable = ['name','price', 'status', 'stock'];
 }
